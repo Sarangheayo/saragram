10165e69c3c41541e5262995369e4bcdbf62abd7
Sarangheayo <myungsoo8504@gmail.com>
2025-11-28 18:00:31 +0900

    251128 server side (app.js, package.json)

--- server/app.js
+++ server/app.js
@@ -12,6 +12,9 @@ import swaggerUi from 'swagger-ui-express';
 import SwaggerParser from 'swagger-parser';
 import path from 'path';
 import filesRouter from './routes/files.router.js';
+import postRouter from './routes/posts.router.js';
+import notFoundRouter from './routes/notFound.router.js';
+import pathUtil from './app/utils/path/path.util.js';
 
 const app = express();
 app.use(express.json()); // json 요청 파싱 처리
@@ -35,7 +38,22 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 // 라우터 정의
 // --------------
 app.use('/api/auth', authRouter);
+app.use('/api/posts', postRouter);
 app.use('/api/files', filesRouter);
+
+// 404 not found 처리
+app.use(notFoundRouter);
+
+// ------------------
+// 뷰 반환 처리
+// ------------------
+// 퍼블릭한 정적파일 제공 활성화
+app.use('/', express.static(process.env.APP_DIST_PATH));
+// React View 반환
+app.get(/^(?!\/files).*/, (req, res) => {
+  return res.sendFile(pathUtil.getViewDirPath());
+  // path.resolve( process.env.APP_DIST_PATH, 'index.html')
+});
 
 // 에러 핸들러 등록
 app.use(errorHandler);
